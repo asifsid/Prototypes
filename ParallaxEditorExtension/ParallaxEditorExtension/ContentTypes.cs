ac54fbb9fdd4c02b770ecafbe850c07babacac38
asifsid <asifsid@live.com>
2015-08-11 17:50:06 -0700

    Initial

---
+++ ParallaxEditorExtension/ParallaxEditorExtension/ContentTypes.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallaxEditorExtension
+{
+    internal static class ContentTypes
+    {
+        [Export]
+        [Name("parallax")]
+        [BaseDefinition("plaintext")]
+        internal static ContentTypeDefinition ParallaxConfigContentTypeDefinition;
+
+        [Export]
+        [FileExtension(".ini")]
+        [ContentType("parallax")]
+        internal static FileExtensionToContentTypeDefinition ParallaxConfigExtensionDefinition;
+    }
+}
